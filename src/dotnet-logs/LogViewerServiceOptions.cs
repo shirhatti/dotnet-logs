879d25675e310f9edec3bcf879bb7d7c91b6d9b9
Sourabh Shirhatti <shirhatti@outlook.com>
2019-10-07 18:13:47 -0700

    Rename to dotnet-logs (#3)
    * Rename project and solution
    
    * Rename to dotnet-logs
    
    * Remove extraneous file

---
+++ src/dotnet-logs/LogViewerServiceOptions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace Microsoft.Diagnostic.Tools.Logs
+{
+    public class LogViewerServiceOptions
+    {
+        public LogViewerServiceOptions()
+        {
+            ProcessId = Process.GetCurrentProcess().Id;
+        }
+        public int ProcessId { get; set; }
+    }
+}
